edbdf85db4bc26cb814625cb1387219809d5815e
Natanael Colacino <natanael.php@gmail.com>
2016-01-20 23:54:25 -0200

    Viewport

--- resources/views/template/template.blade.php
+++ resources/views/template/template.blade.php
@@ -3,12 +3,14 @@
 
 	<head>
 		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width">
+
 		<link rel="styleSheet" type="text/css" href="<?=asset('css/estilo.css')?>">
 		<title>Sistema de Dívidas</title>
 	</head>
 
 	<body>
-		
+
 		@section('beforeCenter')
 		@show
 
